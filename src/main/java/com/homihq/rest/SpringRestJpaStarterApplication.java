b16788cc66a4fc7c0b0e9ddaaf7cffebe46c9621
grabdoc <grabdoc2020@gmail.com>
2023-01-06 22:16:20 -0600

    Initial commit

---
+++ src/main/java/com/homihq/rest/SpringRestJpaStarterApplication.java
@@ -0,0 +1,13 @@
+package com.homihq.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestJpaStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestJpaStarterApplication.class, args);
+    }
+
+}
